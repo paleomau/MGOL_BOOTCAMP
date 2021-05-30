c433ec8d6807b2f100da3f2e2bde65970aec2e26
paleomau <paleomau@gmail.com>
2021-05-30 20:56:04 +0200

    20210530 'Ultimas clases de la semana anterior. Progresos con el proyecto.

--- EDA/src/utils/mining_data_tb.py
+++ EDA/src/utils/mining_data_tb.py
@@ -17,33 +17,30 @@ def all_nan_columns(lista):
 #guardo por posicion no por nombre
 
 
-
+'''
 import pandas as pd
 import numpy as np
-def mineria_pacientes(tpv):
-    pacientes_bruto = pd.read_csv(tpv, sep='\t')
+def mineria_pacientes(tsv):
+    pacientes_bruto = pd.read_csv("../../../../data/" + tsv, sep='\t')
     pacientes_bruto.set_index('track_name', inplace=True)
     pacientes_bruto.drop(['track_type'], axis=1, inplace = True)
     pacientes_bruto
     pacientes_select = pacientes_bruto.loc[['Sex', 'Diagnosis Age', 'Progress Free Survival (Months)', 'Ethnicity Category', 'Mutation Count']]
     pacientes_select = pacientes_select.dropna(axis=1, how='any')
     pacientes = pacientes_select.T
-    return pacientes
+    return pacientes'''
 
-
-import pandas as pd
-import numpy as np
-def mineria_pacientes_hepatic(tpv):
-    pacientes_bruto = pd.read_csv(tpv, sep='\t')
-    pacientes_bruto.set_index('track_name', inplace=True)
-    pacientes_bruto.drop(['track_type'], axis=1, inplace = True)
-    pacientes_bruto
-    pacientes_select = pacientes_bruto.loc[['Sex', 'Diagnosis Age', 'Progress Free Survival (Months)', 'Mutation Count']]
+def mineria_pacientes_parcial(df):
+    df.set_index('track_name', inplace=True)
+    df.drop(['track_type'], axis=1, inplace = True)
+    df
+    pacientes_select = df.loc[['Sex', 'Diagnosis Age', 'Progress Free Survival (Months)', 'Ethnicity Category', 'Mutation Count']]
     pacientes_select = pacientes_select.dropna(axis=1, how='any')
     pacientes = pacientes_select.T
     return pacientes
 
-
+'''
+if __name__ == "__main__":
 ruta = __file__
 print(ruta)
 N = 3  # Mi carpeta EDA raiz está 3 carpetas por encima de mi fichero actual
@@ -53,3 +50,4 @@ for i in range(N):
 print("---------")
 sys.path.append(ruta)
 print(sys.path)
+'''
